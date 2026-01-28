fdd8b27b01612ceb9dc19d89144bbe2acff576d0
HarmishTervadiya <harmishtervadiya@gmail.com>
2026-01-28 21:25:17 +0530

    Implement getContest and getDsaProblemDetails endpoints with error handling

--- assignment1/src/controllers/contest.controller.ts
+++ assignment1/src/controllers/contest.controller.ts
@@ -34,7 +34,39 @@ const createContest = asyncHandler(async (req, res) => {
   return res.status(201).json(new ApiResponse(true, contest, null));
 });
 
-const getContest = asyncHandler(async (req, res) => {});
+const getContest = asyncHandler(async (req, res) => {
+  const { contestId } = req.params;
+  if (!contestId) {
+    return res
+      .status(404)
+      .json(new ApiResponse(false, {}, "Contest not found"));
+  }
+
+  const data = await Promise.all([
+    sql`select * from contests c
+    where c.id=${contestId}`,
+    sql`select * from mcq_questions where contest_id=${contestId}`,
+    sql`select * from dsa_problems where contest_id=${contestId}`,
+  ]);
+
+  if (!data[0] || !data[1] || !data[2]) {
+    return res
+      .status(404)
+      .json(new ApiResponse(false, {}, "Contest not found"));
+  }
+
+  const mcqs = data[1].map(({ correct_option_index, ...rest }) => rest);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        true,
+        { ...data[0][0], mcqs, dsaProblems: data[2] },
+        null,
+      ),
+    );
+});
 
 const addMcqQuestion = asyncHandler(async (req, res) => {
   const { questionText, options, correctOptionIndex, points } = req.body;
@@ -49,25 +81,46 @@ const addMcqQuestion = asyncHandler(async (req, res) => {
     return res.status(400).json(new ApiResponse(false, {}, "BAD_REQUEST"));
   }
 
-  const createdAt= new Date().toISOString()
-  
+  const createdAt = new Date().toISOString();
+
   const insertedMcq = await sql`insert into mcq_questions 
   (contest_id, question_text, options, correct_option_index, points, created_at) values
   (${contestId}, ${questionText}, ${JSON.stringify(options)}, ${correctOptionIndex}, ${points}, ${createdAt})
-  RETURNING *`
+  RETURNING *`;
 
-  if(insertedMcq && !insertedMcq[0]){
-    return res.status(500).json(new ApiResponse(false, {}, "SERVER_ERROR"))
+  if (insertedMcq && !insertedMcq[0]) {
+    return res.status(500).json(new ApiResponse(false, {}, "SERVER_ERROR"));
   }
 
-  return res.status(201).json(new ApiResponse(true, insertedMcq[0], null))  
+  return res.status(201).json(new ApiResponse(true, insertedMcq[0], null));
 });
 
 const submitMcqAnswer = asyncHandler(async (req, res) => {});
 
 const addDsaProblem = asyncHandler(async (req, res) => {});
 
-const getDsaProblemDetails = asyncHandler(async (req, res) => {});
+const getDsaProblemDetails = asyncHandler(async (req, res) => {
+  const { problemId } = req.params;
+
+  if (!problemId) {
+    return res.status(404).json(new ApiResponse(false, {}, null));
+  }
+
+  const data = await Promise.all([
+    sql`select * from dsa_problems where id=${problemId}`,
+    sql`select * from test_cases where problem_id=${problemId}`,
+  ]);
+
+  if (!data[0] || !data[1]) {
+    return res
+      .status(404)
+      .json(new ApiResponse(false, {}, "Dsa problem not found"));
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(true, { ...data[0][0], test_cases: data[1] }, null));
+});
 
 const submitDsaAnswer = asyncHandler(async (req, res) => {});
 
