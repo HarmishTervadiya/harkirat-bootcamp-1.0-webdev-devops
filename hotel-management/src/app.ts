df569c49c100efc062ddcfa57220b6bc61188f0c
HarmishTervadiya <harmishtervadiya@gmail.com>
2026-02-03 18:38:53 +0530

    asyncHandler & ApiResponse setup, custom request property, and prisma setup

--- hotel-management/src/app.ts
+++ hotel-management/src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import { ApiResponse } from "./utils/ApiResponse";
+import { StatusCodes } from "http-status-codes";
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/healthcheck", (req, res) => {
-  res.send("Server is running");
+  return res
+    .status(StatusCodes.OK)
+    .json(new ApiResponse(true, { message: "Server is running!" }, null));
 });
 
-export default app;
+export { app };
--- hotel-management/src/index.ts
+++ hotel-management/src/index.ts
@@ -1,8 +1,6 @@
-import app from "./app";
+import { app } from "./app";
 import { config } from "./config";
 
-app.listen(config.PORT, ()=> {
-    console.log("Server started")
-})
-
-
+app.listen(config.PORT, () => {
+  console.log("Server started");
+});
